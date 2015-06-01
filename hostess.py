13ba5b53852c3f0f1961c59c554cee24486d11f9
cjolsen <christopherjolsen@gmail.com>
2015-05-31 18:59:18 -0600

    basic functionality working

--- hostess.py
+++ hostess.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import os, re
+import os, re, itertools
 
 
 def backup():
@@ -11,6 +11,7 @@ def backup():
     os.makedirs(p, exist_ok=True)
 
     if 'hosts_backup_original' in os.listdir(p):
+        ## TODO: add timestamp to backups
         os.system('cp /etc/hosts ~/.hostess/hosts_backup_recent')
     else:
         os.system('cp /etc/hosts ~/.hostess/hosts_backup_original')
@@ -19,7 +20,7 @@ def backup():
 class Counter(object):
     """
         Creates a helper object for keeping track of rows and columns during
-        the window build
+        the window build.
     """
     
     def __init__(self, start=0):
@@ -39,32 +40,64 @@ class Address(object):
     """
         Holds the websites being blocked.
         Attributes:
-        display: strong.  display name of the website.
-        blocked: boolean.  Is the website currently blocked?
+        display: string, display name of the website.
+        blocked: boolean, is the website currently blocked?
         Methods:
         text: Returns a string ready for writing to /etc/hosts
+        Class Methods:
+        new_from_hosts: takes a string of the form
+            "#127.0.1.1\twww.example.com\n" and builds a new Address object
+        new_from_address: takes a string of the form "www.example.com" and
+            builds a new Address object
+        
     """
-    
-    def __init__(self, raw_text):
-        if re.search(r'(?<=^127.0.1.1\t).*', a).group(0):
-            self.display = re.search(r'(?<=^127.0.1.1\t).*', a).group(0)
-            self.blocked = True
-        elif re.search(r'(?<=^\#127.0.1.1\t).*', a).group(0):
-            self.display = re.search(r'(?<=^\#127.0.1.1\t).*', a).group(0)
-            self.blocked = False
+    def __init__(self, display, blocked=True):
+        self.display = display
+        self.blocked = blocked
+
+    @staticmethod
+    def new_from_host(host_line):
+        """ Takes a line from the hosts file, returns a new Address object. """
+        if re.search(r'(?<=^127.0.1.1\t).*', host_line):
+            # currently blocked
+            display = re.search(r'(?<=^127.0.1.1\t).*', host_line).group(0)
+            blocked = True
+            return Address(display=display, blocked=blocked)
+        elif re.search(r'(?<=^\#127.0.1.1\t).*', host_line):
+            # currently commented out in /etc/hosts
+            display = re.search(r'(?<=^\#127.0.1.1\t).*', host_line).group(0)
+            blocked = False
+            return Address(display=display, blocked=blocked)
         else:
             return None # throw exception?
-
+    
+    @staticmethod
+    def new_from_address(address):
+        """ Takes a web address, creates and returns a new Address object. """
+        # test if real web address
+        return Address(display=address, blocked=True)
+        
     def text(self):
-        """ return ready to be written to /etc/hosts """
+        """
+            Return ready to be written line for /etc/hosts, including tab
+            and newline.
+        """
         if self.blocked == True:
             return ''.join(['127.0.1.1\t', self.display, '\n'])
         else:
             ## unblocked means the line is commented out in /etc/hosts
             return ''.join(['#127.0.1.1\t', self.display, '\n'])
 
+    def set_blocked(self):
+        """ Set the blocked attribute to true. """
+        self.blocked = True
 
-class HostsFile(object):
+    def set_unblocked(self):
+        """ Set the blocked attribute to false. """
+        self.blocked = False
+        
+
+class HostsFileManager(object):
     """
         Object to house all the data from the /etc/hosts file
 
@@ -72,17 +105,20 @@ class HostsFile(object):
         backup: list of hosts file as read in (each line is an item)
         pre_own: list of portion of hosts file before Hostess owned lines
         post_own: list....after Hostess owned lines
-        blocked: list of web urls that are currently being sent to 127.0.1.1
-        unblocked: list of web urls that are commented out in /etc/hosts
+        managed: list of Address objects of managed web addresses
+
     """
+    
     def __init__(self):
         """ Parse the /etc/hosts file and store data in this object. """
         object.__init__(self)
+        self.read()
 
+    def read(self):
         f = open('/etc/hosts', 'r')
         hosts_list = f.readlines()
         self.backup = hosts_list
-        f.close() # it isn't locked anyway...can gksudo be used at open time?
+        f.close() # it isn't locked anyway...
         
         if '# begin Hostess ownership\n' in hosts_list:
             start_ownership = hosts_list.index('# begin Hostess ownership\n')
@@ -91,100 +127,104 @@ class HostsFile(object):
             # save everything before and after the ownership tags
             self.pre_own = hosts_list[:start_ownership]
             self.post_own = hosts_list[end_ownership+1:]
-            owned = hosts_list[start_ownership+1:end_ownership]
+            owned_raw = hosts_list[start_ownership+1:end_ownership]
+            self.managed = [Address.new_from_host(a) for a in owned_raw]
         else:
             self.pre_own = hosts_list
             self.post_own = []
-            owned = []
-        
-        # blocked and unblocked sites are kept as web urls
-        self.blocked = [re.search(r'(?<=^127.0.1.1\t).*', a).group(0)
-                        for a
-                        in owned
-                        if re.search(r'(?<=^127.0.1.1\t).*', a)]
-        self.unblocked = [re.search(r'(?<=\#127.0.1.1\t).*', a).group(0)
-                          for a
-                          in owned
-                          if re.search(r'(?<=\#127.0.1.1\t).*', a)]
-
-        print("\nblocked: ", self.blocked)
-        print("\nunblocked: ", self.unblocked)
-        
-
+            self.managed = []
+    
     def write(self):
         """
             Assemble the text file, write to temp directory, then use
             gksudo to get write privileges to /etc/hosts.
         """
-        pre_own = [''.join([a, '\n'])
+        def handle_newlines(x):
+            if x != '\n':
+                ''.join([x, '\n'])
+            else:
+                '\n'
+        pre_own = [handle_newlines(a) 
                    for a
                    in self.pre_own]
-        post_own = [''.join([a, '\n'])
+        post_own = [handle_newlines(a)
                     for a
                     in self.post_own]
-        blocked = [''.join(['127.0.1.1\t', a, '\n'])
-                   for a
-                   in self.blocked]
-        unblocked = [''.join(['#127.0.1.1\t', a, '\n'])
-                     for a
-                     in self.blocked]
-        
-        if len(post_own) == 0 and len(blocked) == 0 and len(unblocked) == 0:
-            out_text = pre_own
-        else:
-            out_text = ''.join([pre_own,
-                                '# begin Hostess ownership\n',
-                                blocked, unblocked,
-                                '# end Hostess ownership\n',
-                                post_own])
+        owned = [a.text() for a in self.managed]
 
+        print('self.pre_own: ', self.pre_own)
+        print('pre_own: ', pre_own)
+        print('post_own: ', post_own)
+        print('owned: ', owned)
+        
+        out_list = []
+        if len(self.pre_own) > 0:
+            for i in self.pre_own:
+                out_list.append(i)
+        if len(owned) > 0:
+            out_list.append('# begin Hostess ownership\n')
+            for i in owned:
+                out_list.append(i)
+            out_list.append('# end Hostess ownership\n')
+        if len(self.post_own) > 0:
+            for i in self.post_own:
+                out_list.append(i)
+
+        out_text = ''.join(out_list)
         outfile = open('/tmp/temp_hosts.tmp', 'wt')
         outfile.write(out_text)
         outfile.close()
 
         os.system('gksudo mv /tmp/temp_hosts.tmp /etc/hosts')
         
-    def add_blocked(self, address):
-        self.blocked.append(address)
+    def new(self, address):
+        """ Takes a web address and adds it to the managed list. """
+        self.managed.append(Address.new_from_address(address))
 
-    def add_unblocked(self, address):
-        self.unblocked.append(address)
-
-    def remove_blocked(self, address):
-        self.blocked = filter(lambda x: x != address,
-                              self.blocked)
+    def remove(self, address):
+        """ Takes a web address and removes it from the managed list. """
+        self.managed = list(filter(lambda x: x.display != address, self.managed))
         
-    def remove_unblocked(self, address):
-        self.unblocked = filter(lambda x: x != address,
-                                self.unblocked)
-
-    def blocked_to_unblocked(self, address):
-        self.remove_blocked(address)
-        self.add_unblocked(address)
-
-    def unblocked_to_blocked(self, address):
-        self.remove_unblocked(address)
-        self.add_blocked(address)
-
 
 class Application(tk.Frame):
     """
-        Main tkinter object.
+        Main tkinter/GUI object.
     """
     
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
-        self.hosts = HostsFile()
+        self.address_manager = HostsFileManager()
         self.create_widgets()
-        
+    
+    def populate_listbox(self):
+        # separated for DRYness
+        for i in range(len(self.address_manager.managed)):
+            address = self.address_manager.managed[i]
+            self.address_window.insert("end",
+                                       address.display)
+            if address.blocked == True:
+                self.address_window.select_set(i)
+
+    def on_listbox_select(self, event):
+        # for now, just set *all* select states from the address_window
+        # TODO: find which one changed
+        widget = event.widget
+        #index = widget.curselection()[
+        selections = widget.curselection()
+        for i in range(len(self.address_manager.managed)):
+            if i in selections:
+                self.address_manager.managed[i].blocked = True
+            else:
+                self.address_manager.managed[i].blocked = False
+
     def create_widgets(self):
         """
             Build gui widgets and display them.
         """
         
         # these counters help keep track of row and column numbers during
-        # the window build
+        # the window build (can be a big help during interface changes)
         # row.current() gives the current row *without* incrementing the count
         # row.next() increments the count and returns the new value
         # row.reset(X) resets the row to X and returns X (useful for columns)
@@ -192,88 +232,61 @@ class Application(tk.Frame):
         col = Counter()
         
         # make widgets
-        self.blocked_label = tk.Label(self, text="Blocked pages")
-        self.blocked_window = tk.Listbox(self, selectmode="multiple")
-        for i in self.hosts.blocked:
-            self.blocked_window.insert("end", i)
-
-        self.remove_button = tk.Button(self, text="Remove selected",
+        self.address_label = tk.Label(self, text="Blocked pages (Grey=blocked, White=not blocked)")
+        self.address_window = tk.Listbox(self, selectmode="multiple", width=60)
+        # populate Listbox
+        self.populate_listbox()
+        # make buttons
+        self.save_button = tk.Button(self, text="Save",
+                                     command=self.commit_changes)
+        self.remove_button = tk.Button(self, text="Remove Selected",
                                        command=self.remove)
-        self.add_new_button = tk.Button(self, text="Add new",
+        self.add_new_button = tk.Button(self, text="Add New",
                                         command=self.add_new)
         self.add_new_text = tk.Entry(self)
-
-        self.move_down_button = tk.Button(self, text="Unblock Selected")
-        self.move_up_button = tk.Button(self, text="Block Selected")
-        
-        self.unblocked_label = tk.Label(self, text="Unblocked pages")
-        self.unblocked_window = tk.Listbox(self, selectmode="multiple")
-        for j in self.hosts.unblocked:
-            self.unblocked_window.insert("end", j)
-
-        self.save_button = tk.Button(self,
-                                    text="Save Changes",
-                                    command=self.commit_changes)
-        self.reset_button = tk.Button(self,
-                                     text="Reset",
-                                     command=self.reset)
         
         # display widgets
-        self.blocked_label.grid(row=row.current(),
+        self.address_label.grid(row=row.current(),
                                 column=col.current())
-        self.blocked_window.grid(row=row.next(),
-                                 column=col.current(), columnspan=3)
-        self.remove_button.grid(row=row.next(),
-                                column=col.current())
+        self.address_window.grid(row=row.next(),
+                                 column=col.current(), columnspan=4)
+        self.save_button.grid(row=row.next(),
+                              column=col.current())
+        self.remove_button.grid(row=row.current(),
+                                column=col.next())
         self.add_new_button.grid(row=row.current(),
                                  column=col.next())
         self.add_new_text.grid(row=row.current(),
                                column=col.next())
-        col.reset()
-        self.move_down_button.grid(row=row.next(),
-                                   column=col.current())
-        self.move_up_button.grid(row=row.current(),
-                                 column=col.next())
-        col.reset()
-        self.unblocked_label.grid(row=row.next(),
-                                  column=col.current())
-        self.unblocked_window.grid(row=row.next(),
-                                   column=col.current(), columnspan=3)
-        self.save_button.grid(row=row.next(), column=col.current())
-        self.reset_button.grid(row=row.current(), column=col.next())
-        
-    def remove(self, web_url):
-        """ Remove item from blocked list. web_url: string. """
-        self.blocked = filter(lambda x: x != web_url, self.blocked)
-        
-    def add_new(self, web_url):
-        """ Add new item to blocked list. web_url: string. """
-        self.blocked.append(web_url)
+
+        # bind events
+        self.address_window.bind('<<ListboxSelect>>', self.on_listbox_select)
+
+    def remove(self):
+        """ Remove active item from listbox. web_url: string. """
+        active = self.address_window.get("active")
+        self.address_manager.remove(active)
+        self.refresh()
     
-    def move_down(self, web_url):
-        """
-            Delete item from blocked list and add it to ublocked list.
-            web_url: string.
-        """
-        self.blocked = filter(lambda x: x != web_url, self.blocked)
-        self.unblocked.append(web_url)
-    
-    def move_up(self):
-        """
-            Delete item from blocked list and add it to ublocked list.
-            web_url: string.
-        """
-        self.unblocked = filter(lambda x: x != web_url, self.blocked)
-        self.blocked.append(web_url)
-    
+    def add_new(self):
+        """ Add new item to blocked list. """
+        self.address_manager.new(self.add_new_text.get())
+        self.refresh()
+
+    def refresh(self):
+        """ Refresh the address listbox with data from the address_manager """
+        self.address_window.delete(0, "end")
+        self.populate_listbox()
+  
     def reset(self):
         """ Reload data from /etc/hosts """
-        pass
+        self.address_manager = HostsFileManager()
+        self.refresh()
 
     def commit_changes(self):
         """ Gather data from gui and commit to /etc/hosts """
-        #self.blocked = 
-        self.hosts.write()
+        self.address_manager.write()
+
 
 backup()
 app = Application()
